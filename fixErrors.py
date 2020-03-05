5c24288c7e9ffa8dd64e655916df98dd7f098922
Andrei Shpilenok <shpilenok2001@gmail.com>
2020-03-05 00:14:09 -0800

    removed unnecessary graphs, improved fixErrors.py

--- fixErrors.py
+++ fixErrors.py
@@ -3,47 +3,60 @@ import networkx as nx
 import matplotlib.pylab as plt
 import wptools
 
-G = nx.read_gml("Graphs/FullNoRedirects2.gml")
-
-def removeNonValidAirports():
+def removeNonValidAirports(G, verbose=False):
     for i, node in enumerate(list(G.nodes(data=True))):
         if "IATA" not in node[1]:
-            print(i, node[0], len(G.edges(node[0])), end=" ")
+            if verbose:
+                print(i, node[0], len(G.edges(node[0])), end=" ")
             try:
                 page = wptools.page(node[0], silent=True).get_parse()
                 iata = page.data["infobox"]["IATA"]
-                print(iata, end=" ")
-                if iata == "none" or iata == "pending":
+                if verbose:
+                    print(iata, end=" ")
+                if "none" in iata.lower() or "pending" in iata.lower() or "n/a" in iata.lower() or "<s>" in iata.lower():
                     raise TypeError
             except (LookupError, TypeError) as e:
-                print("\033[31mNOT VALID PAGE/NO IATA CODE!\033[0m", end="")
+                if verbose:
+                    print("\033[31mNOT VALID PAGE/NO IATA CODE!\033[0m", end="")
                 G.remove_node(node[0])
-            print()
+            if verbose:
+                print()
 
-    # Draw it. Prettyyyy
-    pos = nx.get_node_attributes(G, 'Position')
-    nx.draw(G, pos=pos)
-    labels = nx.get_node_attributes(G, 'IATA')
-    nx.draw_networkx_labels(G, pos=pos, labels=labels)
-    nx.write_gml(G, "Graphs/FullNoRedirectsFixedSomeErrors.gml")
-    plt.show()
 
-def findAirpotsWithNoDestinations():
+def findAirpotsWithNoDestinations(G, verbose=False):
     count = 0
     for i, node in enumerate(list(G.nodes)):
         if len(G.edges(node)) == 0:
-            print(i, node)
+            if verbose:
+                print(i, node)
             count += 1
             G.remove_node(node)
-    print("TOTAL:", count)
+    if verbose:
+        print("\033[31mTOTAL:", count, "\033[0m")
+
+def removeHeliports(G, verbose=False):
+    count = 0
+    for i, node in enumerate(list(G.nodes)):
+        if "heliport" in node.lower():
+            if verbose:
+                print("REMOVED", i, node)
+            G.remove_node(node)
+            count += 1
+    if verbose:
+        print("\033[31mREMOVED", count, "HELIPORTS\033[0m")
+
+def main(G=nx.read_gml("Graphs/FullNoRedirects2.gml")):
+    findAirpotsWithNoDestinations(G, True)
+    removeHeliports(G, True)
+    removeNonValidAirports(G, True)
 
     # Draw it. Prettyyyy
     pos = nx.get_node_attributes(G, 'Position')
     nx.draw(G, pos=pos)
     labels = nx.get_node_attributes(G, 'IATA')
     nx.draw_networkx_labels(G, pos=pos, labels=labels)
-    nx.write_gml(G, "Graphs/FullNoRedirectsFixedSomeErrorsRemovedNoDestinations.gml")
+    nx.write_gml(G, "Graphs/FullNoRedirectsRemovedWrong&Heliports&NoDest.gml")
     plt.show()
-    
+
 if __name__ == "__main__":
-    findAirpotsWithNoDestinations()
+    main()
